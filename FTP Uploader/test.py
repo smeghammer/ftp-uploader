17bb37aa82507c34767509fc6cbc91e982ccb9f6
Silas <silas@thecoven.co.uk>
2021-01-18 10:42:35 +0000

    first commit

---
+++ FTP Uploader/test.py
@@ -0,0 +1,27 @@
+'''
+Created on 17 Jan 2021
+
+@author: smegh
+
+See https://www.tutorialspoint.com/python3/python_gui_programming.htm
+'''
+import tkinter # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
+top = tkinter.Tk()
+# Code to add widgets will go here...
+
+# File drop area
+
+
+# File list (of those dropped)
+
+
+# upload go button
+
+
+# configurable FTP target
+
+
+# Config editor?
+
+
+top.mainloop()
