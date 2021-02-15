0a623f8d4f52b1522eb40157b8909eba43e39bca
smeghammer <smeghammer@live.com>
2021-02-15 15:12:16 +0000

    refactor and fix drag and drop on Ubuntu

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ Created on 9 Jan 2021
 see https://www.devdungeon.com/content/python-ftp-client-tutorial#toc-16
 '''
 import argparse
-import importlib
+import configparser
 from ftplib import FTP
 import os
 
@@ -14,31 +14,53 @@ import os
 '''
 capture CLI args:
 '''
-parser = argparse.ArgumentParser(description='Start metadata collection with startnode, db server, db port and DB name')
-# Required args:
-parser.add_argument( '-f', '--file', help='file to upload', type=str, required=True)
+parser = argparse.ArgumentParser(description='Simple FTP uploader')
+parser.add_argument( '-f', '--file', help='file to upload (single file only)', type=str, required=True)
 args = parser.parse_args()
 
-def test(file):
-    print(file)
+def upload(file):
+    print("starting...")
+    try:
+        print("trying...")
+        config = configparser.RawConfigParser()
+        config.sections()
+        config.read('config.ini') #this will fail if no config.ini found
+        print("config read OK")
+        '''
+        Instantiate FTP instance with target and credentials; and set target path:
+        '''
+        print(config)
+        ftp = FTP(config['ftp']['target'], config['user']['login'], config['user']['pass'])
+        print(ftp)
+        ftp.cwd(config['ftp']['path'])
+        print(ftp)
+        
+        res = ftp.retrlines('LIST')
+        print(res)
+        #text mode
+        try:
+            text_file = open(file, 'rb')
+            #TODPO: intercept here if no input file found:
+    #         ftp.storlines('STOR '+file, text_file)
+            print(file.split('\\')[-1])
+            '''
+            Split the filepath to extract the actual file. Note: OS independent:
+            '''
+            ftp.storbinary('STOR '+file.split(os.path.sep)[-1], text_file)
+        except FileNotFoundError as err:
+            print('input file not found: {0}'.format(str(err)))
+        except Exception as err:
+            print('an ftp error occurred: {0}'.format(str(err)))
+        
+        finally:
+            ftp.quit()
+    except FileNotFoundError as err:
+        print('no config file found! ({0})'.format(str(err)))
+    except Exception as err:
+        print('a general error occurred: {0}'.format(str(err)))
     
-    '''
-    for ID Games:
-    '''
-    ftp = FTP('archives.gamers.org','anonymous','your_email@your_host.com')
-    ftp.cwd('pub/idgames/incoming')
-    
-
-    
-    res = ftp.retrlines('LIST')
-    print(res)
-    #text mode
-    with open(file, 'rb') as text_file:
-#         ftp.storlines('STOR '+file, text_file)
-        print(file.split('\\')[-1])
-        ftp.storbinary('STOR '+file.split(os.path.sep)[-1], text_file)
- 
-    ftp.quit()
+    finally:
+        print("Done")
 
 '''
 Entry point:
@@ -46,6 +68,6 @@ Entry point:
 if __name__ == '__main__':
     print('starting with ' + args.file)
     
-    ''' Here I load a crawler based on the passed arg:  '''
-    test(args.file)
+    ''' call upload function with file path '''
+    upload(args.file)
     
---
+++ uploader.idgames.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# define where your deployment is:
+WORKING_DIRECTORY=/path/to/location/ftp-uploader/
+
+#echo the passed filename:
+echo $1
+
+#change shell file execution context to the directory in which the uploader is deployed:
+cd $WORKING_DIRECTORY
+
+#Add your python3 alias here:
+python3.6 ${WORKING_DIRECTORY}start.py -f "$1"
+
+#wait for a keypress to exit (you can comment this if you want):
+echo "Press any key to exit"
+read -n 1 -s
